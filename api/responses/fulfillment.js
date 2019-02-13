48c4c390b46c17cc97bc2153e5e0d8e720faf50b
Martin Choy <mtchoy@comp.hkbu.edu.hk>
2019-02-13 23:18:49 +0800

    found `isPermissionGranted`

--- api/responses/fulfillment.js
+++ api/responses/fulfillment.js
@@ -43,7 +43,7 @@ module.exports = function () {
 
         let conv = agent.conv();
         let params = agent.parameters;
-        let permissionGranted = agent.permissionGranted;
+        let permissionGranted = agent.isPermissionGranted();
 
         if (!permissionGranted) {
             conv.close('Sorry, permission denied.');
