b532fce75109b162ea2f3cf0b7fd80d2b9a6fc4b
Martin Choy <mtchoy@comp.hkbu.edu.hk>
2019-02-14 00:51:14 +0800

    bug fixed

--- api/policies/fulfillment.js
+++ api/policies/fulfillment.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-const functions = require('firebase-functions');
+// const functions = require('firebase-functions');
 const { WebhookClient } = require('dialogflow-fulfillment');
 const { Card, Suggestion } = require('dialogflow-fulfillment');
 const { dialogflow, Permission, BasicCard, Button, Image } = require('actions-on-google');
