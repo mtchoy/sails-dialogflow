f091f75eb3094730c15875920982c506b1739001
Martin Choy <mtchoy@comp.hkbu.edu.hk>
2019-02-13 22:18:45 +0800

    developed `userGrantsPermission` handler

--- api/responses/fulfillment.js
+++ api/responses/fulfillment.js
@@ -1,51 +1,134 @@
 
 module.exports = function () {
 
-  var req = this.req;
-  var res = this.res;
+    var req = this.req;
+    var res = this.res;
 
-  const { WebhookClient } = require('dialogflow-fulfillment');
-  const { Card, Suggestion } = require('dialogflow-fulfillment');
-  const { dialogflow, Permission, BasicCard, Button, Image } = require('actions-on-google');
+    const { WebhookClient } = require('dialogflow-fulfillment');
+    const { Card, Suggestion } = require('dialogflow-fulfillment');
+    const { dialogflow, Permission, BasicCard, Button, Image } = require('actions-on-google');
 
-  const agent = new WebhookClient({ request: req, response: res });
+    const agent = new WebhookClient({ request: req, response: res });
 
-  function welcome(agent) {
-    agent.add(`Welcome to my agent!`);
-  }
-
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-  }
-
-  // app.intent('User provides type', (conv, params) => {
-
-  function userProvidesType(agent) {
-
-    let conv = agent.conv();
-    let params = agent.parameters;
-
-    if (["Drink Carton", "Coffee Capsule"].includes(params.wasteType)) {
-      conv.close(`This can't be recycled!!!`);
-      return agent.end(conv);
+    function welcome(agent) {
+        agent.add(`Welcome to my agent!`);
     }
 
-    conv.data.requestedPermission = 'DEVICE_PRECISE_LOCATION';
+    function fallback(agent) {
+        agent.add(`I didn't understand`);
+        agent.add(`I'm sorry, can you try again?`);
+    }
 
-    conv.ask(new Permission({
-      context: `${params.wasteType}? Cool. To locate you`,
-      permissions: conv.data.requestedPermission,
-    }));
-    return agent.add(conv);
+    // app.intent('User provides type', (conv, params) => {
 
-  }
+    function userProvidesType(agent) {
 
-  let intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
-  intentMap.set('User provides type', userProvidesType );
-  // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
-  // intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
-  agent.handleRequest(intentMap);
+        let conv = agent.conv();
+        let params = agent.parameters;
+
+        if (["Drink Carton", "Coffee Capsule"].includes(params.wasteType)) {
+            conv.close(`This can't be recycled!!!`);
+            return agent.end(conv);
+        }
+
+        conv.data.requestedPermission = 'DEVICE_PRECISE_LOCATION';
+
+        conv.ask(new Permission({
+            context: `${params.wasteType}? Cool. To locate you`,
+            permissions: conv.data.requestedPermission,
+        }));
+        return agent.add(conv);
+
+    }
+
+    function userGrantsPermission(agent) {
+
+        let conv = agent.conv();
+        let params = agent.parameters;
+        let permissionGranted = agent.permissionGranted;
+
+        if (!permissionGranted) {
+            conv.close('Sorry, permission denied.');
+            return agent.end(conv);
+        }
+
+        const { requestedPermission } = conv.data;
+
+        if (requestedPermission !== 'DEVICE_PRECISE_LOCATION') {
+            conv.close('Sorry, I could not figure out where you are.');
+            return agent.end(conv);
+        }
+
+        const { coordinates } = conv.device.location;
+
+        if (coordinates) {
+
+            // return conv.close(`You are at ${coordinates.latitude}, ${coordinates.longitude}`);
+
+            const wasteType = conv.contexts.get('userprovidestype-followup').parameters.wasteType;
+
+            var url = `https://api.data.gov.hk/v1/nearest-recyclable-collection-points?lat=${coordinates.latitude}&long=${coordinates.longitude}&max=30`;
+
+            var options = {
+                method: 'get',
+                uri: url,
+            };
+
+            return request(options)
+                .then(body => {
+
+                    var parsed = JSON.parse(body);
+                    console.log(parsed.results.length);
+
+                    var nearest_cp = null;
+
+                    parsed.results.some(function (cp) {
+
+                        if (cp["waste-type"].indexOf(wasteType) != -1) {
+                            nearest_cp = cp;
+                            return true;    // breaking the some loop
+                        }
+
+                    });
+
+                    if (!nearest_cp) {
+                        conv.close("No collection point nearby.");
+                        return agent.end(conv);
+                    }
+
+                    conv.close(`The nearest collection point is at ${nearest_cp.address_en}, it's one for ${nearest_cp.waste_type}.`);
+
+                    conv.close(new BasicCard({
+                        buttons: new Button({
+                            title: 'See it on Google Map',
+                            url: `https://www.google.com/maps/search/?api=1&query=${nearest_cp.lat},${nearest_cp.lgt}`,
+                        }),
+                        image: new Image({
+                            url: 'https://img.icons8.com/color/1600/google-maps.png',
+                            alt: 'Image alternate text',
+                        }),
+                        display: 'CROPPED',
+                    }));
+
+                    return agent.end(conv);
+
+                })
+                .catch(err => {
+                    // You should also return a message here of some sort
+                    conv.close("Error fetching data from Gov.hk");
+                    return agent.end(conv)
+                });
+
+        }
+
+    };
+
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('User provides type', userProvidesType);
+    intentMap.set('User grants permission', userGrantsPermission);
+    // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
+    // intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
+    agent.handleRequest(intentMap);
 }
