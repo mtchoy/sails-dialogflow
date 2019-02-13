53c27d2e75619e2d21c04fd11bb2b2a6b59d0955
Martin Choy <mtchoy@comp.hkbu.edu.hk>
2019-02-13 23:50:56 +0800

    return app;

--- api/controllers/FulfillmentController.js
+++ api/controllers/FulfillmentController.js
@@ -4,6 +4,7 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
+
 module.exports = {
   index: async function (req, res) {
     return res.fulfillment();
